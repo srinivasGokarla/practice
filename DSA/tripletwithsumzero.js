ed98fcbf28dd10024c69b2e6f0b967d56fc8ce52
srinivasGokarla <srinivasgokarla@gmail.com>
2022-11-19 23:57:59 +0530

    dsa

--- DSA/tripletwithsumzero.js
+++ DSA/tripletwithsumzero.js
@@ -1,7 +1,7 @@
 function runProgram(input) {
     input = input.trim().split("\n")
      let n = Number(input[0])
-     let arr = input[1].trim().split(" ").map(Number).sort((a,b)=> a-b)
+     let arr = input[1].trim().split(" ").map(Number)
     //  let count = 0;
     //  let sum = 0
     //  for(let i = 0; i < arr.length - 2; i++) {
@@ -15,40 +15,67 @@ function runProgram(input) {
         
     // }
     // console.log(count)
+    /*function runProgram(input){
+    input=input.split("\n");
+    let[n,k] = input[0].trim().split(" ").map(Number)
+    let arr = input[1].trim().split(" ").map(Number)
+console.log(countTriplets(n,k,arr))
+}
+function countTriplets(n,k,arr){
+      
+        arr.sort(function(a,b){return b-a});
+
+        let ans = 0;
+        for (let i = 0; i < n - 2; i++){
+            let j = i + 1, k = n - 1;
+            while (j < k){
+            
+                if (arr[i] + arr[j] + arr[k] >= sum)
+                    k--;
+                else
+                {
+                    ans += (k - j);
+                    j++;
+                }
+            }
+        }
+        return ans;
+    }
+   */
   
-    let sum = 0
-    console.log(triplet(arr,n))
+  
+    (triplet(arr,n))
    
 }
 function triplet(arr,n) {
-  for (let i = 0; i < n - 1; i++) {
-      
-    let l = i + 1;
-    let r = n - 1;
-    let x = arr[i];
-    let sum = 0
-    let count = 0;
-    while (l < r) {
-    
-        if (x + arr[l] + arr[r] === sum){
-         // console.log(x,arr[l], arr[r])
-          count++;
-            l++;
-            r--;
-        } else if (x + arr[l] + arr[r] < sum)
-            l++;
+  let res = [];
+  let sum = 0
+  for(i=0;i<arr.length;i++){
+      for(j=0;j<arr.length;j++){
+          for(k=0;k<arr.length;k++){
+              if(arr[i]+arr[j]+arr[k] === sum){
+                 
+             res.push(arr[i])
+             res.push(arr[j]) 
+             res.push(arr[k])   
+                  break;
+              }
+          }
+      }
+  }
 
-        else
-            r--;
-    }
-    return count
+if(res===[]){
+  console.log(false)
+}else{
+  console.log(true)
+  console.log(res[0],res[1],res[2])
 }
 
 }
 
     if (process.env.USERNAME === "srini") {
-      runProgram(`3
-      -7 1 5 2 -4 3 0`);
+      runProgram(`4
+      -1 -1 -2 3`);
     } else {
       process.stdin.resume();
       process.stdin.setEncoding("ascii");
