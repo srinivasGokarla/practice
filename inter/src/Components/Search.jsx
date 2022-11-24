29dcb7fcb54c020eb1a44ee3e45b501172fbf3a4
srinivasGokarla <srinivasgokarla@gmail.com>
2022-11-24 21:04:55 +0530

    search

---
+++ inter/src/Components/Search.jsx
@@ -0,0 +1,46 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+export default function Search() {
+  const itemList = [
+    "Apple",
+    "Orange",
+    "Banana",
+    "Cherry",
+    "Milk",
+    "Peanuts",
+    "Butter",
+    "Tomato"
+  ];
+
+  const [filteredList, setFilteredList] = new useState(itemList);
+
+  const filterBySearch = (event) => {
+    // Access input value
+    const query = event.target.value;
+    // Create copy of item list
+    var updatedList = [...itemList];
+    // Include all elements which includes the search query
+    updatedList = updatedList.filter((item) => {
+      return item.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+    });
+    // Trigger render with updated values
+    setFilteredList(updatedList);
+  };
+
+  return (
+    <div className="App">
+      <div className="search-header">
+        <div className="search-text">Search:</div>
+        <input id="search-box" onChange={filterBySearch} />
+      </div>
+      <div id="item-list">
+        <ol>
+          {filteredList.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
+}
