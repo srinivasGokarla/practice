e8e7b2b1b4d7bb5bc24cf9cd962aed01bb242f93
Gokarla Srinivas <87422030+srinivasGokarla@users.noreply.github.com>
2023-07-24 11:57:53 +0530

    Update firstbigger.js

--- DSA/firstbigger.js
+++ DSA/firstbigger.js
@@ -1,54 +1,54 @@
 
-//   function runProgram(input) {
-//     input = input.split("\n");
-//    let N = Number(input[0])
-//    let arr = input[1].trim().split(" ").map(Number)
-//    let test = Number(input[2])
-//    let line = 3;
-//    for(let i= 0; i < test; i++) {
-//       let min = Number(input[line++])
-//      // console.log(test,arr,N,min)
-//     let res = FirstBigger(arr,N,min)
-//     if(res === undefined) {
-//         console.log("-1")
-//     } else {
-//         console.log(res)
-//     }
+  function runProgram(input) {
+    input = input.split("\n");
+   let N = Number(input[0])
+   let arr = input[1].trim().split(" ").map(Number)
+   let test = Number(input[2])
+   let line = 3;
+   for(let i= 0; i < test; i++) {
+      let min = Number(input[line++])
+     // console.log(test,arr,N,min)
+    let res = FirstBigger(arr,N,min)
+    if(res === undefined) {
+        console.log("-1")
+    } else {
+        console.log(res)
+    }
     
-//    }
-// }
-// function FirstBigger(arr,N,min) {
-//   let ans = []
-//     for(let i= 0; i < N; i++) {
-//       if(arr[i] > min) {
-//           ans.push(arr[i])
+   }
+}
+function FirstBigger(arr,N,min) {
+  let ans = []
+    for(let i= 0; i < N; i++) {
+      if(arr[i] > min) {
+          ans.push(arr[i])
       
-//       } 
-//     }
-//    // console.log(ans)
-//     let small = ans[0]
-//     for(let i= 0; i < ans.length; i++) {
-//         if(small > ans[i]) {
-//             small = ans[i]
+      } 
+    }
+   // console.log(ans)
+    let small = ans[0]
+    for(let i= 0; i < ans.length; i++) {
+        if(small > ans[i]) {
+            small = ans[i]
            
-//         } 
-//     }
-//    return small
-// }
-// if (process.env.USERNAME === 'srini') {
-//   runProgram(`10
-//   13 89 81 66 81 63 71 76 73 81
-//   2
-//   65
-//   15
-//   `);
-// } else {
-//   process.stdin.resume();
-//   process.stdin.setEncoding("ascii");
-//   let read = "";
-//   process.stdin.on("data", function (input) {
-//     read += input;
-//   });
+        } 
+    }
+   return small
+}
+if (process.env.USERNAME === 'srini') {
+  runProgram(`10
+  13 89 81 66 81 63 71 76 73 81
+  2
+  65
+  15
+  `);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
 
   
  
